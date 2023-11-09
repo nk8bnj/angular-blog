f7adf9c9ed9368491d0678f8795476f424745d3a
Nikita Ganzha <geekneek28@gmail.com>
2023-11-09 13:06:34 +0200

    added shared module, auth service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { EditPageComponent } from './edit-page/edit-page.component'
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component'
 import { LoginPageComponent } from './login-page/login-page.component'
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component'
 import { CreatePageComponent } from './create-page/create-page.component'
-import { EditPageComponent } from './edit-page/edit-page.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AuthService } from './shared/services/auth.service'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '',
@@ -47,5 +51,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
     ]),
   ],
   exports: [RouterModule],
+  providers: [AuthService],
 })
 export class AdminModule { }
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { User } from 'src/app/shared/interface'
+import { AuthService } from '../shared/services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login-page',
@@ -7,7 +10,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-  form!: FormGroup;
+  form!: FormGroup
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -16,12 +24,22 @@ export class LoginPageComponent implements OnInit {
         Validators.required,
         Validators.minLength(6),
       ]),
-    });
+    })
   }
 
   submit() {
     if (this.form.invalid) {
-      return;
+      return
     }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }
